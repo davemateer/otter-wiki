6a59e28236ca647bd586f07b5ac59d76539a95f0
DaveMateerWork@gmail.com <DaveMateerWork@gmail.com@4ce9a6fc-e524-c23a-28e9-a39800725a02>
2013-04-09 17:48:41 +0000

    initial development

---
+++ src/Mvc/Scripts/main.js
@@ -0,0 +1,21 @@
+﻿$(document).ready(function () {
+  $('#add-article-link').button({
+    icons: {
+      primary: "ui-icon-circle-plus"
+    }
+  });
+
+  $('.edit-button').button({
+    icons: {
+      primary: "ui-icon-pencil"
+    }
+  });
+
+  $('.history-button').button({
+    icons: {
+      primary: "ui-icon-clock"
+    }
+  });
+
+  $('#search-form input.query-submit').button();
+});
