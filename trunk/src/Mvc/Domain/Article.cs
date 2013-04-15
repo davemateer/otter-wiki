c1387ec1d8a4d6e33d4d1418214a68a21af8fb49
DaveMateerWork@gmail.com <DaveMateerWork@gmail.com@4ce9a6fc-e524-c23a-28e9-a39800725a02>
2013-04-15 17:10:21 +0000

    initial development

--- trunk/src/Mvc/Domain/Article.cs
+++ trunk/src/Mvc/Domain/Article.cs
@@ -1,12 +1,9 @@
 ﻿namespace Otter.Domain
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Web;
-
     public sealed class Article : ArticleBase
     {
         public string Html { get; set; }
+
+        public string UrlTitle { get; set; }
     }
 }
