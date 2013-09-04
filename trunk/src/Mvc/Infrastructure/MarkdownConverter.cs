a291e4df452eda25bc753b434a466e7a006ef2e1
DaveMateerWork@gmail.com <DaveMateerWork@gmail.com@4ce9a6fc-e524-c23a-28e9-a39800725a02>
2013-09-04 14:22:54 +0000

    Add ability to render tables using Mediawiki markup.

--- trunk/src/Mvc/Infrastructure/MarkdownConverter.cs
+++ trunk/src/Mvc/Infrastructure/MarkdownConverter.cs
@@ -7,6 +7,10 @@ namespace Otter.Infrastructure
     using System.Web;
     using MarkdownSharp;
     using HtmlAgilityPack;
+    using System.Text.RegularExpressions;
+    using System.Text;
+    using System.Xml;
+    using System.Diagnostics;
 
     public sealed class MarkdownConverter : ITextToHtmlConverter
     {
@@ -34,15 +38,254 @@ namespace Otter.Infrastructure
             list.Add("ul", null);
             list.Add("br", null);
             list.Add("hr", null);
+
+            list.Add("table", null);
+            list.Add("thead", null);
+            list.Add("tbody", null);
+            list.Add("tr", null);
+            list.Add("td", null);
+            list.Add("th", null);
+
             return list;
         }
 
         public string Convert(string plainText)
         {
             var markdown = new Markdown();
-            string html = markdown.Transform(plainText);
+            string html = Regex.Replace(plainText, @"^\s*\{\|.*\|}\s*$", new MatchEvaluator(RenderTable), RegexOptions.Singleline | RegexOptions.Multiline);
+            html = markdown.Transform(html);
             html = Purify(html);
             return html;
+        }
+
+        private static string RenderTable(Match match)
+        {
+            var cellContent = new StringBuilder();
+
+            int columnCountVerify = 0;
+            int currentRowColumnCount = 0;
+
+            var context = new Stack<string>();
+
+            var table = new StringBuilder();
+            var settings = new XmlWriterSettings();
+            settings.OmitXmlDeclaration = true;
+            var writer = XmlWriter.Create(table, settings);
+
+            var lines = Regex.Split(match.Value, "\r?\n");
+            foreach (string line in lines)
+            {
+                string trimmed = line.TrimStart(null);
+
+                if (trimmed.StartsWith("{|", StringComparison.OrdinalIgnoreCase))
+                {
+                    Debug.Assert(context.Count == 0, "context.Count == 0");
+                    if (context.Count > 0)
+                    {
+                        return match.Value;
+                    }
+
+                    writer.WriteStartElement("table");
+                    context.Push("table");
+                }
+                else if (trimmed.StartsWith("|}"))
+                {
+                    if (context.Peek() == "td")
+                    {
+                        WriteCellContent(writer, cellContent, context);
+                        currentRowColumnCount++;
+                    }
+
+                    Debug.Assert(context.Peek() == "tr", "context.Peek() == \"tr\"");
+                    if (context.Peek() != "tr")
+                    {
+                        return match.Value;
+                    }
+
+                    if (columnCountVerify > 0 && currentRowColumnCount != columnCountVerify)
+                    {
+                        return match.Value;
+                    }
+
+                    writer.WriteEndElement();  // tr
+                    context.Pop();
+
+                    Debug.Assert(context.Peek() == "tbody", "context.Peek() == \"tbody\"");
+                    if (context.Peek() != "tbody")
+                    {
+                        return match.Value;
+                    }
+
+                    writer.WriteEndElement();  // tbody
+                    context.Pop();
+
+                    Debug.Assert(context.Peek() == "table", "context.Peek() == \"table\"");
+                    if (context.Peek() != "table")
+                    {
+                        return match.Value;
+                    }
+
+                    writer.WriteEndElement();  // table
+                    context.Pop();
+                }
+                else if (trimmed.StartsWith("|-"))
+                {
+                    if (context.Peek() == "td" || context.Peek() == "th")
+                    {
+                        WriteCellContent(writer, cellContent, context);
+                        currentRowColumnCount++;
+                    }
+
+                    if (context.Peek() == "tr")
+                    {
+                        if (columnCountVerify > 0 && currentRowColumnCount != columnCountVerify)
+                        {
+                            return match.Value;
+                        }
+
+                        columnCountVerify = currentRowColumnCount;
+                        currentRowColumnCount = 0;
+                        writer.WriteEndElement();  // tr
+                        context.Pop();
+                    }
+
+                    if (context.Peek() != "tbody")
+                    {
+                        if (context.Peek() == "thead")
+                        {
+                            writer.WriteEndElement();  // thead
+                            context.Pop();
+                        }
+
+                        writer.WriteStartElement("tbody");
+                        context.Push("tbody");
+                    }
+
+                    writer.WriteStartElement("tr");
+                    context.Push("tr");
+                }
+                else if (trimmed.StartsWith("!"))
+                {
+                    if (context.Peek() == "table")
+                    {
+                        writer.WriteStartElement("thead");
+                        context.Push("thead");
+
+                        writer.WriteStartElement("tr");
+                        context.Push("tr");
+                    }
+                    else if (context.Peek() == "td" || context.Peek() == "th")
+                    {
+                        WriteCellContent(writer, cellContent, context);
+                        currentRowColumnCount++;
+                    }
+
+                    string content = trimmed.Substring(1);
+
+                    string[] columns = content.Split(new string[] { "!!" }, StringSplitOptions.None);
+                    if (columns.Length == 1)
+                    {
+                        writer.WriteStartElement("th");
+                        context.Push("th");
+                        cellContent.AppendLine(content);
+                    }
+                    else
+                    {
+                        foreach (var column in columns)
+                        {
+                            writer.WriteStartElement("th");
+                            writer.WriteString(column.Trim());
+                            writer.WriteEndElement();  // th
+                            currentRowColumnCount++;
+                        }
+                    }
+                }
+                else if (trimmed.StartsWith("|"))
+                {
+                    if (context.Peek() == "table")
+                    {
+                        writer.WriteStartElement("tbody");
+                        context.Push("tbody");
+
+                        writer.WriteStartElement("tr");
+                        context.Push("tr");
+                    }
+                    else if (context.Peek() == "td" || context.Peek() == "th")
+                    {
+                        WriteCellContent(writer, cellContent, context);
+                        currentRowColumnCount++;
+                    }
+
+                    string content = trimmed.Substring(1);
+
+                    string[] columns = content.Split(new string[] { "||" }, StringSplitOptions.None);
+                    if (columns.Length == 1)
+                    {
+                        writer.WriteStartElement("td");
+                        context.Push("td");
+                        cellContent.AppendLine(content);
+                    }
+                    else
+                    {
+                        foreach (var column in columns)
+                        {
+                            writer.WriteStartElement("td");
+                            writer.WriteString(column.Trim());
+                            writer.WriteEndElement();  // td
+                            currentRowColumnCount++;
+                        }
+                    }
+                }
+                else
+                {
+                    if (context.Count == 0)
+                    {
+                        // leading or trailing blank line; ignore
+                        continue;
+                    }
+
+                    Debug.Assert(context.Peek() == "td" || context.Peek() == "th", "context.Peek() == \"td\" || context.Peek() == \"th\"");
+                    if (context.Peek() != "td" && context.Peek() != "th")
+                    {
+                        return match.Value;
+                    }
+
+                    cellContent.AppendLine(trimmed);
+                }
+            }
+
+            Debug.Assert(context.Count == 0, "context.Count == 0");
+            if (context.Count > 0)
+            {
+                return match.Value;
+            }
+
+            writer.Flush();
+            return table.ToString();
+        }
+
+        private static void WriteCellContent(XmlWriter writer, StringBuilder content, Stack<string> context)
+        {
+            while (content.Length > 0 && Char.IsWhiteSpace(content[content.Length - 1]))
+            {
+                content.Remove(content.Length - 1, 1);
+            }
+
+            string html = string.Empty;
+            if (content.Length == 0)
+            {
+                html = "&nbsp;";
+            }
+            else
+            {
+                var markdown = new Markdown();
+                html = markdown.Transform(content.ToString()).TrimEnd(null);
+            }
+
+            writer.WriteRaw(html);
+            content.Clear();
+            writer.WriteEndElement();  // td or th
+            context.Pop();
         }
 
         private static string Purify(string html)
---
+++ trunk/src/Test/Infrastructure/MarkdownConverterTest.cs
@@ -0,0 +1,72 @@
+﻿namespace Test
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Otter.Infrastructure;
+
+    [TestClass]
+    public class MarkdownConverterTest
+    {
+        [TestMethod]
+        public void Convert_Table()
+        {
+            var converter = new MarkdownConverter();
+            var actual = converter.Convert("{|\n|Orange\n|Apple\n|-\n|Bread\n|Pie\n|-\n|Butter\n|Ice Cream \n|}");
+            string expected = "<table><tbody><tr><td><p>Orange</p></td><td><p>Apple</p></td></tr><tr><td><p>Bread</p></td><td><p>Pie</p></td></tr><tr><td><p>Butter</p></td><td><p>Ice Cream</p></td></tr></tbody></table>";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Convert_TableSingleLineRows()
+        {
+            var converter = new MarkdownConverter();
+            var actual = converter.Convert("{|\n|Orange||Apple||more\n|-\n|Bread||Pie||more\n|-\n|Butter||Ice Cream||and more\n|}");
+            string expected = "<table><tbody><tr><td>Orange</td><td>Apple</td><td>more</td></tr><tr><td>Bread</td><td>Pie</td><td>more</td></tr><tr><td>Butter</td><td>Ice Cream</td><td>and more</td></tr></tbody></table>";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Convert_TableIgnoreExtraSpacing()
+        {
+            var converter = new MarkdownConverter();
+            var actual = converter.Convert("{|\n|  Orange    ||   Apple   ||   more\n|-\n|   Bread    ||   Pie     ||   more\n|-\n|   Butter   || Ice cream ||  and more\n|}");
+            string expected = "<table><tbody><tr><td>Orange</td><td>Apple</td><td>more</td></tr><tr><td>Bread</td><td>Pie</td><td>more</td></tr><tr><td>Butter</td><td>Ice cream</td><td>and more</td></tr></tbody></table>";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Convert_TableLongText()
+        {
+            var converter = new MarkdownConverter();
+            var actual = converter.Convert("{|\n|Lorem ipsum dolor sit amet,  \nconsetetur sadipscing elitr, \nsed diam nonumy eirmod tempor invidunt\nut labore et dolore magna aliquyam erat, \nsed diam voluptua. \n\nAt vero eos et accusam et justo duo dolores\net ea rebum. Stet clita kasd gubergren,\nno sea takimata sanctus est Lorem ipsum\ndolor sit amet. \n|\n- Lorem ipsum dolor sit amet\n- consetetur sadipscing elitr\n- sed diam nonumy eirmod tempor invidunt\n|}");
+            string expected = "<table><tbody><tr><td><p>Lorem ipsum dolor sit amet,<br>\nconsetetur sadipscing elitr, \nsed diam nonumy eirmod tempor invidunt\nut labore et dolore magna aliquyam erat, \nsed diam voluptua. </p>\n\n<p>At vero eos et accusam et justo duo dolores\net ea rebum. Stet clita kasd gubergren,\nno sea takimata sanctus est Lorem ipsum\ndolor sit amet.</p></td><td><ul>\n<li>Lorem ipsum dolor sit amet</li>\n<li>consetetur sadipscing elitr</li>\n<li>sed diam nonumy eirmod tempor invidunt</li>\n</ul></td></tr></tbody></table>";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Convert_TableHeaders()
+        {
+            var converter = new MarkdownConverter();
+            var actual = converter.Convert("{|\n! Item\n! Amount\n! Cost\n|-\n|Orange\n|10\n|7.00\n|-\n|Bread\n|4\n|3.00\n|-\n|Butter\n|1\n|5.00\n|-\n!Total\n|\n|15.00\n|}");
+            string expected = "<table><thead><tr><th><p>Item</p></th><th><p>Amount</p></th><th><p>Cost</p></th></tr></thead><tbody><tr><td><p>Orange</p></td><td><p>10</p></td><td><p>7.00</p></td></tr><tr><td><p>Bread</p></td><td><p>4</p></td><td><p>3.00</p></td></tr><tr><td><p>Butter</p></td><td><p>1</p></td><td><p>5.00</p></td></tr><tr><th><p>Total</p></th><td>&nbsp;</td><td><p>15.00</p></td></tr></tbody></table>";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Convert_TableForbidInbalancedTables()
+        {
+            var converter = new MarkdownConverter();
+            var actual = converter.Convert("{|\n|Orange||Apple||more\n|-\n|Bread||Pie||more\n|-\n|Butter||and more\n|}");
+            string expected = "<p>{|\n|Orange||Apple||more\n|-\n|Bread||Pie||more\n|-\n|Butter||and more\n|}</p>";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Convert_MixedTable()
+        {
+            var converter = new MarkdownConverter();
+            var actual = converter.Convert("Interface servers\n-----------------\n\n{|\n! Server !! Purpose\n|-\n| IF-HVB-03 || MCAP Dictation Interface\n|}");
+            string expected = "<h2>Interface servers</h2>\n\n<table><thead><tr><th>Server</th><th>Purpose</th></tr></thead><tbody><tr><td>IF-HVB-03</td><td>MCAP Dictation Interface</td></tr></tbody></table>";
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
