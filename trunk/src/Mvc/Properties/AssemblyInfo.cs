a28c584e7cf50225e7deffd484e2fe2922f5e686
DaveMateerWork@gmail.com <DaveMateerWork@gmail.com@4ce9a6fc-e524-c23a-28e9-a39800725a02>
2014-04-16 14:15:58 +0000

    Add favicon

--- trunk/src/Mvc/Properties/AssemblyInfo.cs
+++ trunk/src/Mvc/Properties/AssemblyInfo.cs
@@ -56,5 +56,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.43.0.0")]
-[assembly: AssemblyFileVersion("0.43.0.0")]
+[assembly: AssemblyVersion("0.44.0.0")]
+[assembly: AssemblyFileVersion("0.44.0.0")]
